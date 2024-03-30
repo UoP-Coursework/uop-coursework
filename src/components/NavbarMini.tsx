39263c02993d9f11e689c964a677b7687df138c1
Jpuf0 <jpuf@jpuf.xyz>
2024-03-30 16:13:50 +0000

    refactor(app): changes some stuff with linting
    fixes some linting errors

--- src/components/NavbarMini.tsx
+++ src/components/NavbarMini.tsx
@@ -13,7 +13,7 @@ import classes from "../styles/navbarmin.module.css";
 import { useRouter } from "next/router";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
-import { Session } from "next-auth";
+import type { Session } from "next-auth";
 import React from "react";
 
 interface NavbarLinkProps {
@@ -24,10 +24,10 @@ interface NavbarLinkProps {
   onClick?(): void;
 }
 
-const isIconType = (x: any): x is IconType => !React.isValidElement(x);
+const isIconType = (x: unknown): x is IconType => !React.isValidElement(x);
 
 function genUserIcon(session: Session) {
-  return <Image src={session.user.image || "no-user.png"} alt="no-user" height={128} width={128} className="rounded-md w-8 h-8"/>
+  return <Image src={session.user.image ?? "no-user.png"} alt="no-user" height={128} width={128} className="rounded-md w-8 h-8"/>
 }
 
 function NavbarLink({ icon: Icon, label, active, onClick }: NavbarLinkProps) {
